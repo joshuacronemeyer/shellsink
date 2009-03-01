0149f26cf2b4ca0ed77aa411b68ef369cfdcb843
jcroneme <admin@aubergine>
2009-02-28 22:24:59 -0600

    basic project housekeeping.  getting gpl license notice on all source files that I wrote...  didn't know if I should bother with the Makefiles so i didn't.

--- server/sysadmin.py
+++ server/sysadmin.py
@@ -1,3 +1,20 @@
+"""
+This file is part of Shell-Sink.
+Copyright Joshua Cronemeyer 2008, 2009
+
+Shell-Sink is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Shell-Sink is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License v3 for more details.
+
+You should have received a copy of the GNU General Public License
+along with Shell-Sink.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import hashlib
 from time import time
 from google.appengine.ext import db
